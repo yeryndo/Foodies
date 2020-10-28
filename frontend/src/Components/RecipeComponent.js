9b9c052a7018466795f510028e394c252d1425a0
blu101 <billylu0102@gmail.com>
2020-10-28 19:34:58 -0400

    added new card component and fixed styling

--- frontend/src/Components/RecipeComponent.js
+++ frontend/src/Components/RecipeComponent.js
@@ -1,57 +1,117 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import './Grid/recipeDisplay.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography';
+import {red} from '@material-ui/core/colors'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import ShareIcon from '@material-ui/icons/Share'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import RecipeService from '../Services/RecipeService';
-
-class RecipeComponent extends React.Component {
-    
-    constructor(props){
-        super(props)
-        this.state = {            
-            recipes:[],
-            meal: this.props.meal,
-            category: this.props.category
-        }
-    }
-
-    componentDidMount(){
-        RecipeService.getRecipes(this.state.meal, this.state.category).then((response) => {
-            this.setState({ recipes: response.data})
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container';
+import './Grid/recipeDisplay.css';
+ 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: window.innerWidth / 2, 
+      marginLeft: 20,
+      marginRight: 10,
+      marginBottom: 30,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%',
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
+ 
+  export default function RecipeCard(props){
+      const classes = useStyles();
+      const [expanded, setExpanded] = React.useState(false);
+      const[recipes, setRecipe] = React.useState([])
+      useEffect(() =>{
+        RecipeService.getRecipes(props.meal, props.category).then((response) => {
+             setRecipe(response.data)
         });
-    }
-
-    render(){
-        return(
-            <div>
-                <Container fluid>
-                    <Row>
-                        {this.state.recipes.map((recipe, index) => {
-                            return <div className = "indvCard">
-                                <Col>
-                                    <CardDeck>
-                                        <Card>
-                                            <Card.Img variant="top" src={recipe.imageUrl} />
-                                            <Card.Body>
-                                                <Card.Title>{recipe.name}</Card.Title>
-                                                    <Card.Text>
-                                                        {recipe.detail}
-                                                    </Card.Text>                        
-                                            </Card.Body>
-                                        </Card>         
-                                    </CardDeck>
-                                </Col>
-                            </div>
-                        })}
-                    </Row>     
-                </Container>
-            </div>
-        )
-    }
-}
-
-export default RecipeComponent
+      })
+      const handleExpandClick = () =>{
+          setExpanded(!expanded);
+      };
+ 
+      return(
+        <Container fluid>
+            <Row>
+                {recipes.map((recipe, index) => {
+                    return <div className = "indvCard">
+                        <Card className={classes.root}>
+                            <CardHeader
+                                action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                                }
+                                title={recipe.name}
+                                // subheader="October 22, 2020" {date}
+                            />
+                            <CardMedia 
+                                className={classes.media}
+                                image={recipe.imageUrl}
+                                title={recipe.name}
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {recipe.detail}
+                                </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                {/* <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                    <ShareIcon />
+                                </IconButton> */}
+                                <IconButton
+                                    className={clsx(classes.expand, {
+                                        [classes.expandOpen]: expanded,
+                                    })}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <Typography paragraph>Test:</Typography>
+                                    <Typography paragraph>This is a test for the new recipe card</Typography>              
+                                </CardContent>
+                            </Collapse>
+                        </Card>
+                    </div>
+            })}
+            </Row>
+        </Container>
+      )
+  }
