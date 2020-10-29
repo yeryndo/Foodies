07c817712c99d4255905152d9c742e70bb587340
Yeryn Do <yeryn.do99@gmail.com>
2020-10-29 17:42:49 -0400

    Fixed h2 error in fruit page

--- frontend/src/EntreeScreen/Breakfast/FruitBreakfastScreen.js
+++ frontend/src/EntreeScreen/Breakfast/FruitBreakfastScreen.js
@@ -4,7 +4,7 @@ import RecipeComponent from "../../Components/RecipeComponent";
 function FruitBreakfastScreen() {
   return (
     <div>
-      <h2> style={{ padding: 30 }}List of Fruit Recipes for Breakfast</h2>
+      <h2 style={{ padding: 30 }}>List of Fruit Recipes for Breakfast</h2>
       <RecipeComponent meal ="breakfast" category="fruit"/>
     </div>
   );
